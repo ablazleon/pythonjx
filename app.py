b49555b14be0e7abbb4d7f1b2c779eb49a323580
Adrián <adrian.blazquez.leon@alumnos.upm.es>
2020-06-26 13:05:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ class MyHandler(BaseHTTPRequestHandler):
     self.send_header('Content-type','text/html')
     self.end_headers()
     # Send the html message
-    output = 'v1 - This is Adrian'
+    output = 'v1'
     self.wfile.write(output.encode('utf-8'))
-  return
+    return
 
   try:
     server = HTTPServer(('', PORT_NUMBER), MyHandler)
