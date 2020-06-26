1e348340c52035180f32612f7bb24ab44f5c55f1
Adrián <adrian.blazquez.leon@alumnos.upm.es>
2020-06-26 13:02:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,19 @@ PORT_NUMBER = 8080
 class MyHandler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-    """Handler for GET requests"""
     self.send_response(200)
-    self.send_header('Content-type','image/png')
+    self.send_header('Content-type','text/html')
     self.end_headers()
-    with open('logo.png', 'rb') as f:
-      self.wfile.write(f.read())
+    # Send the html message
+    output = 'v1 - This is Adrian'
+          self.wfile.write(output.encode('utf-8'))
+  return
 
-try:
-  server = HTTPServer(('', PORT_NUMBER), MyHandler)
-  print('Started httpserver on port', PORT_NUMBER)
-  server.serve_forever()
+  try:
+    server = HTTPServer(('', PORT_NUMBER), MyHandler)
+    print('Started httpserver on port', PORT_NUMBER)
+    server.serve_forever()
 
-except KeyboardInterrupt:
-  server.server_close()
-  print('Stopping server')
+  except KeyboardInterrupt:
+    server.server_close()
+    print('Stopping server')
